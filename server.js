ddbed08f79ece21a49e84d11dc78eb5302bcc055
Rokiatou <rouattara15@gmail.com>
2020-02-29 22:12:23 +0000

    first GET route

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name: 'Roki'},
+    {name: 'Kutchi'}
+]
 
 
 app.listen(8000, function(){
     console.log("Server is running");
+})
+
+app.get('/Users', function(req, res){
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
 })
