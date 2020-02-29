2a2632b91c1a3ba93c6b72b874ed71d7643e2892
Rokiatou <rouattara15@gmail.com>
2020-02-29 23:14:27 +0000

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 const mockUserData = [
     {name: 'Roki'},
     {name: 'Kutchi'}
 ]
+
 
 
 app.listen(8000, function(){
@@ -18,5 +23,27 @@ app.get('/Users/:id', function(req, res){
         users: req.params.id    
     })
     console.log(req.params.id);
+})
+
+app.get('/login', function(req, res){
+    const userName = req.body.userName;
+    const userPass = req.body.userPass;
+
+    const mockUserName = "Rokiatou";
+    const mockUserPass = "Code";
+    
+    if(userName == mockUserName && userPass == mockUserPass){
+        res.json({
+            success: true,
+            message: "Vos donnees sont correctes",
+            token: 'encrypted token goes here'
+        })   
+    }
+    else{
+        res.json({
+            success: false,
+            message: "Vos donnees ne sont pas correctes"
+        })
+    }   
 })
 
